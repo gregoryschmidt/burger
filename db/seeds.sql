885e1791a7aed802645a636d7cda96c0839e75c9
GREGORY SCHMIDT <gregoryschmidt@GREGORYs-MBP.webpass.net>
2019-03-15 23:32:08 -0700

    intial

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name)
+VALUES ("Double Quarter Pouder with Cheese"),
+("ShackBurger"),
+("Lounge Burger"),
+("Double Charburger"),
+("Jumbo Jack Cheeseburger"),
+("Double Whopper");
